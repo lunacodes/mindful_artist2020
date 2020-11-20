8d0221d7a5b79109c9569fee33e645c38778ef49
Luna <lunacodes@gmail.com>
2020-11-20 03:43:12 -0500

    Add custom funcs & styles. Separate Mailchimp code

--- themes/TheMindFulArtist/functions.php
+++ themes/TheMindFulArtist/functions.php
@@ -2,17 +2,20 @@
 /**
  * Roots includes
  */
-require_once locate_template('/lib/utils.php');           // Utility functions
-require_once locate_template('/lib/init.php');            // Initial theme setup and constants
-require_once locate_template('/lib/sidebar.php');         // Sidebar class
-require_once locate_template('/lib/config.php');          // Configuration
-require_once locate_template('/lib/activation.php');      // Theme activation
-require_once locate_template('/lib/cleanup.php');         // Cleanup
-require_once locate_template('/lib/nav.php');             // Custom nav modifications
-// require_once locate_template('/lib/comments.php');        // Custom comments modifications
-require_once locate_template('/lib/rewrites.php');        // URL rewriting for assets
-require_once locate_template('/lib/htaccess.php');        // HTML5 Boilerplate .htaccess
-require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
-require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
-require_once locate_template('/lib/custom.php');          // Custom functions
-require_once('aq_resizer.php');						      // Handles Image Resizing
+require_once locate_template( '/lib/utils.php' );           // Utility functions
+require_once locate_template( '/lib/init.php' );            // Initial theme setup and constants
+require_once locate_template( '/lib/sidebar.php' );         // Sidebar class
+require_once locate_template( '/lib/config.php' );          // Configuration
+require_once locate_template( '/lib/activation.php' );      // Theme activation
+require_once locate_template( '/lib/cleanup.php' );         // Cleanup
+require_once locate_template( '/lib/nav.php' );             // Custom nav modifications
+// require_once locate_template( '/lib/comments.php' );        // Custom comments modifications
+require_once locate_template( '/lib/rewrites.php' );        // URL rewriting for assets
+require_once locate_template( '/lib/htaccess.php' );        // HTML5 Boilerplate .htaccess
+require_once locate_template( '/lib/widgets.php' );         // Sidebars and widgets
+require_once locate_template( '/lib/scripts.php' );         // Scripts and stylesheets
+require_once locate_template( '/lib/custom.php' );          // Custom functions
+require_once( 'aq_resizer.php' );                 // Handles Image Resizing
+require_once( get_template_directory() . '/inc/custom-functions.php' );                  // Additional custom functions
+
+// require_once( get_template_directory_uri() . '/inc/custom-functions.php' );                  // Additional custom functions
---
+++ themes/TheMindFulArtist/inc/custom-functions.php
@@ -0,0 +1,7 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'enqueue_custom_styles' );
+function enqueue_custom_styles() {
+	wp_enqueue_style( 'mailchimp-classic-10-7', '//cdn-images.mailchimp.com/embedcode/classic-10_7.css' );
+	wp_enqueue_style( 'custom-styles', '/wp-content/themes/TheMindfulArtist/inc/css/custom-styles.css' );
+}
